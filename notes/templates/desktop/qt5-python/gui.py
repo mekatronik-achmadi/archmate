fe8f9c06ec0fcf9d831a68e3a6cf1451dd9da19c
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2024-01-11 08:51:31 +0700

    Template Python Qt

---
+++ notes/templates/desktop/qt5-python/gui.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import QMainWindow, QMessageBox
+from Ui_gui import Ui_Gui
+import sys
+
+class Gui(QMainWindow,Ui_Gui):
+
+    def __init__(self,parent=None):
+        super(Gui, self).__init__(parent)
+        self.setupUi(self)
+
+    @pyqtSlot()
+    def on_btnMsg_clicked(self):
+       msgBox = QMessageBox()
+       msgBox.setIcon(QMessageBox.Information)
+       msgBox.setWindowTitle("Template")
+       msgBox.setText("Template Python Qt")
+       msgBox.setStandardButtons(QMessageBox.Ok)
+       msgBox.exec()
+
+    @pyqtSlot()
+    def on_btnQuit_clicked(self):
+        sys.exit()
+
