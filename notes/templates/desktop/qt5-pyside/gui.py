6e5f04fb70d798b9c93f1e3e856e3abb2d92668c
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2024-01-13 06:36:54 +0700

    Template PySide Qt

---
+++ notes/templates/desktop/qt5-pyside/gui.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+import sys
+from PySide2.QtCore import Slot
+from PySide2.QtWidgets import QMainWindow,QMessageBox
+from Ui_gui import Ui_Gui
+
+class Gui(QMainWindow,Ui_Gui):
+    def __init__(self, parent=None):
+        super(Gui, self).__init__(parent)
+        self.setupUi(self)
+
+    @Slot()
+    def on_btnMsg_clicked(self):
+        msgbox = QMessageBox()
+        msgbox.setIcon(QMessageBox.Information)
+        msgbox.setWindowTitle("Template")
+        msgbox.setText("Template Qt PySide")
+        msgbox.setStandardButtons(QMessageBox.Ok)
+        msgbox.exec()
+
+    @Slot()
+    def on_btnQuit_clicked(self):
+        sys.exit()
+
