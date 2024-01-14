18a5e2654e016f136d49fcf10e3ba0abf845bbd8
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2024-01-14 16:37:51 +0700

    Template GTK NodeJS

--- notes/templates/desktop/gtk-nodejs/main.js
+++ notes/templates/desktop/gtk-nodejs/main.js
@@ -1,37 +1,35 @@
-#!/usr/bin/node
-
 // commands to run:
 //npm install
 //node main.js
 
-const path = require('path')
-const gi = require('node-gtk')
-const Gtk = gi.require('Gtk','3.0')
+const path = require('path');
+const gi = require('node-gtk');
+const Gtk = gi.require('Gtk','3.0');
 
-gi.startLoop()
-Gtk.init()
+gi.startLoop();
+Gtk.init();
 
-const uiFile = path.join(__dirname,'main.glade')
-const builder = Gtk.Builder.newFromFile(uiFile)
+const uiFile = path.join(__dirname,'main.glade');
+const builder = Gtk.Builder.newFromFile(uiFile);
 
 const handlers = {
     mBtnMsg_clicked_cb: function() {
         let msgbox = new Gtk.MessageDialog({
             text: 'Template NodeJS GTK',
             buttons: Gtk.ButtonsType.OK,
-        })
-        msgbox.run()
-        msgbox.destroy()
+        });
+        msgbox.run();
+        msgbox.destroy();
     },
-    mBtnQuit_clicked_cb: Gtk.mainQuit
-}
+    mBtnQuit_clicked_cb: Gtk.mainQuit,
+};
 
-builder.connectSignals(handlers)
+builder.connectSignals(handlers);
 
-const win = builder.getObject('mWnd')
-win.setDefaultSize(200,100)
-win.setResizable(false)
+const win = builder.getObject('mWnd');
+win.setDefaultSize(200,100);
+win.setResizable(false);
 
-win.showAll()
-Gtk.main()
+win.showAll();
+Gtk.main();
 
