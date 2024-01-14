8f5de4b93e8bf601f9830d8a71e66cf842413539
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2024-01-14 16:23:03 +0700

    Template Gtk NodeJS no server

--- notes/templates/desktop/gtk-nodejs/main.js
+++ notes/templates/desktop/gtk-nodejs/main.js
@@ -4,20 +4,6 @@
 //npm install
 //node main.js
 
-//////////////////// Simple Server /////////////////
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/',(req,res) => {
-    res.send('NodeJS Template')
-})
-
-server = app.listen(port,() => {
-    console.log(`Running server on port ${port} closed`)
-});
-
-//////////////////// Desktop Gtk /////////////////
 const path = require('path')
 const gi = require('node-gtk')
 const Gtk = gi.require('Gtk','3.0')
@@ -28,13 +14,6 @@ Gtk.init()
 const uiFile = path.join(__dirname,'main.glade')
 const builder = Gtk.Builder.newFromFile(uiFile)
 
-function appClose(){
-    server.close(() => {
-        console.log(`Express Server on port ${port} closed`)
-    })
-    Gtk.mainQuit()
-}
-
 const handlers = {
     mBtnMsg_clicked_cb: function() {
         let msgbox = new Gtk.MessageDialog({
@@ -44,7 +23,7 @@ const handlers = {
         msgbox.run()
         msgbox.destroy()
     },
-    mBtnQuit_clicked_cb: appClose,
+    mBtnQuit_clicked_cb: Gtk.mainQuit
 }
 
 builder.connectSignals(handlers)
@@ -55,3 +34,4 @@ win.setResizable(false)
 
 win.showAll()
 Gtk.main()
+
