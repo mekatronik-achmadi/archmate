f18b14cc3c978235361e667cb9994434061b627c
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2024-02-10 16:08:58 +0700

    Serial Not Work in SimulIDE

--- notes/templates/chips/m8/main.c
+++ notes/templates/chips/m8/main.c
@@ -1,12 +1,77 @@
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <util/delay.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define CHARLEN     16
+#define BAUDRATE    9600
+
+char inString[CHARLEN];
+
+FILE uart_in = {0};
+FILE uart_out = {0};
+
+static int uart_getchar(FILE *stream){
+    (void) stream;
+    char tmp;
+
+    while ((UCSRA & (1<<UDRE))==0) {};
+    tmp=UDR;
+
+    return tmp;
+}
+
+static int uart_putchar(char chr, FILE *stream){
+    if(chr=='\n') uart_putchar('\r',stream);
+
+    while ((UCSRA & (1<<UDRE))==0) {};
+    UDR=chr;
+
+    return 0;
+}
+
+static void uart_init(void){
+    DDRD |= 1<<1;
+
+    UCSRB |= 1<<RXEN | 1<< TXEN;
+    UCSRB |= 1<<RXCIE;
+    UCSRC |= 1<<UCSZ0 | 1<<UCSZ1;
+
+    UBRRH = ((F_CPU/(BAUDRATE*16UL))-1)>>8;
+    UBRRL = ((F_CPU/(BAUDRATE*16UL))-1);
+
+    fdev_setup_stream(&uart_in,NULL,uart_getchar,_FDEV_SETUP_READ);
+    fdev_setup_stream(&uart_out,uart_putchar,NULL,_FDEV_SETUP_WRITE);
+
+    sei();
+}
 
 int main(void)
 {
-    DDRC |= 1<<6;
+    DDRD |= 1<<3;
+
+    uart_init();
+    printf("Setup Done\n");
+
     while (1) {
-       PORTC ^= 1<<6;
+       PORTD ^= 1<<3;
+       printf("Serial OK\n");
        _delay_ms(500);
     }
     return 0;
 }
+
+ISR(USART_RXC_vect){
+    scanf("%s",inString);
+
+    if(strcmp(inString,"test")==0){
+        printf("Serial OK\n");
+    }
+    else{
+        printf("%s?\n",inString);
+    }
+}
+
