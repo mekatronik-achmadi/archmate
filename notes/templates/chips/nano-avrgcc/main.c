e107bcc80df761f50be3000e6c7759378ab60529
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2024-02-10 18:19:07 +0700

    AVR GCC Template

--- notes/templates/chips/nano-avrgcc/main.c
+++ notes/templates/chips/nano-avrgcc/main.c
@@ -18,17 +18,17 @@ static int uart_getchar(FILE *stream){
     (void) stream;
     char tmp;
 
-    while ((UCSRA & (1<<UDRE))==0) {};
-    tmp=UDR;
+    while ((UCSR0A & (1<<RXC0))==0) {};
+    tmp=UDR0;
 
-    return tmp;
+    return(tmp);
 }
 
 static int uart_putchar(char chr, FILE *stream){
     if(chr=='\n') uart_putchar('\r',stream);
 
-    while ((UCSRA & (1<<UDRE))==0) {};
-    UDR=chr;
+    while ((UCSR0A & (1<<UDRE0))==0) {};
+    UDR0=chr;
 
     return 0;
 }
@@ -36,42 +36,42 @@ static int uart_putchar(char chr, FILE *stream){
 static void uart_init(void){
     DDRD |= 1<<1;
 
-    UCSRB |= 1<<RXEN | 1<< TXEN;
-    UCSRB |= 1<<RXCIE;
-    UCSRC |= 1<<UCSZ0 | 1<<UCSZ1;
+    UCSR0B |= 1<<RXEN0 | 1<< TXEN0;
+    UCSR0B |= 1<<RXCIE0;
+    UCSR0C |= 1<<UCSZ00 | 1<<UCSZ01;
 
-    UBRRH = ((F_CPU/(BAUDRATE*16UL))-1)>>8;
-    UBRRL = ((F_CPU/(BAUDRATE*16UL))-1);
+    UBRR0H = ((F_CPU/(BAUDRATE*16UL))-1)>>8;
+    UBRR0L = ((F_CPU/(BAUDRATE*16UL))-1);
 
     fdev_setup_stream(&uart_in,NULL,uart_getchar,_FDEV_SETUP_READ);
+    stdin = &uart_in;
+
     fdev_setup_stream(&uart_out,uart_putchar,NULL,_FDEV_SETUP_WRITE);
+    stdout = &uart_out;
 
     sei();
 }
 
 int main(void)
 {
-    DDRD |= 1<<3;
+    DDRB |= 1<<5; // Arduino: D13
 
     uart_init();
     printf("Setup Done\n");
 
     while (1) {
-       PORTD ^= 1<<3;
-       printf("Serial OK\n");
-       _delay_ms(500);
+       PORTB ^= 1<<5;
+       _delay_ms(200);
     }
     return 0;
 }
 
-ISR(USART_RXC_vect){
+ISR(USART_RX_vect){
     scanf("%s",inString);
 
-    if(strcmp(inString,"test")==0){
-        printf("Serial OK\n");
-    }
-    else{
-        printf("%s?\n",inString);
-    }
+    if(strcmp(inString,"test")==0) printf("Serial OK\n");
+    else printf("%s?\n",inString);
+
+    return;
 }
 
