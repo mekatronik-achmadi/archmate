aa888f744588a673ad8af1250d43361b8641f499
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2024-03-11 18:41:28 +0700

    Rust Examples

--- notes/templates/desktop/rust-fltk/build.sh
+++ notes/templates/desktop/rust-fltk/build.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 # prepare
-cargo add gtk
+cargo add fltk
 
 # to run
 cargo build
-cp -vf src/main.glade ./target/debug/
 
 # using built binary
 #./target/debug/gtk_template
---
+++ notes/templates/desktop/rust-fltk/src/main.rs
@@ -0,0 +1,7 @@
+extern crate fltk;
+
+use fltk::{app, prelude::*, window::Window};
+
+fn main() {
+    let app = app::App::default();
+}
