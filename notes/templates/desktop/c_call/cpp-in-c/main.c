676b020a982c8101b50646d8a59f55b6c3284408
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2024-03-15 22:59:17 +0700

    Valgrind not work

--- notes/templates/desktop/c_call/cpp-in-c/main.c
+++ notes/templates/desktop/c_call/cpp-in-c/main.c
@@ -9,13 +9,11 @@ int main(int argc, char *argv[])
     uint16_t res;
 
     pPwr objPwr = Pwr_new();
-    char *tesStr = (char*) malloc(16 * sizeof(char));
 
     res = Pwr_power(objPwr,4,3);
     printf("Power C from C++: %i\r\n",res);
 
     Pwr_del(objPwr);
-    //free(tesStr);
 
     return 0;
 }
